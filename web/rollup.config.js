4ace8c19a3389e7d981646f428380506f5c1d122
konsti219 <37149441+konsti219@users.noreply.github.com>
2020-12-05 12:48:35 +0000

    Revert "env change 2"
    This reverts commit 6e9f2fbce290da6244e67a9c0d4e98f56395b82d.

--- web/rollup.config.js
+++ web/rollup.config.js
@@ -4,11 +4,13 @@ import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import css from "rollup-plugin-css-only";
 import { terser } from "rollup-plugin-terser";
+import dotenv from "dotenv"
+dotenv.config({ path: "../" });
 
 export default {
   input: "src/main.js",
   output: {
-    sourcemap: true,
+    sourcemap: process.env.NODE_ENV = "development",
     format: "iife",
     name: "vr_video_player_web",
     file: "public/build/bundle.js",
